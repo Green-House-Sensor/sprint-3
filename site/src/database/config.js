b3df2fbed7b1933f4b1f522ca94b91335a89ce52
victxrfreitas <maciel.freitas@sptech.school>
2022-05-27 13:41:23 -0300

    arduino att

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "aluno",
     database: "greenhouse",
-    password: "#Gf45016792801",
+    password: "sptech",
 };
 
 function executar(instrucao) {
