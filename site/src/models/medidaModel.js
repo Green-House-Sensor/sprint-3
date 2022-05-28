2bd414f3f626a3293fc332dbbbdce5145d7e1917
YuzoTaka <henrique.takahashi@sptech.school>
2022-05-28 15:56:36 -0300

    dashboard 3 graficos

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idEstufa) {
-    instrucaoSql = `SELECT idEstufa, idDado, dht11Umidade AS umidade, lm35Temperatura AS temperatura, luminosidade, momento, DATE_FORMAT  (momento,'%H:%i:%s') FROM dado
+    instrucaoSql = `SELECT idEstufa, idDado, dht11Umidade AS umidade, lm35Temperatura AS temperatura, luminosidade, momento, DATE_FORMAT  (momento,'%H:%i:%s') AS momento FROM dado
                     JOIN sensor ON fkSensor = idSensor
                     JOIN estufa ON fkEstufa = idEstufa where idEstufa = ${idEstufa}
                     order by idDado desc limit 7;`;
